40f6a04f7f6c505569f5ca67198504457442690c
ian <iancmilan@unochapeco.edu.br>
2021-01-02 18:11:13 -0300

    autenticação 🔑

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: '88b2aa9b9b5cab2e84f63aba79be8b81',
+        expiresIn: '1d', // 1 dia
+    },
+};
---
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+
+interface TokenPayload {
+    iat: number;
+    exp: number;
+    sub: string;
+}
+
+export default function ensureAuthenticated(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+): void {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        throw new Error('JWT token is missing.');
+    }
+
+    // Essa vírgula indica que vou utilizar somente a segunda posição do array, que será o token
+    const [, token] = authHeader.split(' ');
+
+    try {
+        const decoded = verify(token, authConfig.jwt.secret);
+
+        const { sub } = decoded as TokenPayload;
+
+        // Essa variável user originalmente não existe no express, o que foi feito pra contornar isso foi criar o arquivo express.d.ts dentro da pasta @types para fazer um overwrite na tipagem
+        request.user = {
+            id: sub,
+        };
+
+        return next();
+    } catch {
+        throw new Error('Invalid JWT token');
+    }
+}
