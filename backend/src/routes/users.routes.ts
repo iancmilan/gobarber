45d48853fc0c000b950646a9a9e8efbd679d3891
ian <iancmilan@unochapeco.edu.br>
2021-01-06 11:19:42 -0300

    tratando exceções ☠️

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -11,26 +11,22 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-    try {
-        const { name, email, password } = request.body;
+    const { name, email, password } = request.body;
 
-        const createUser = new CreateUserService();
+    const createUser = new CreateUserService();
 
-        const user = await createUser.execute({ name, email, password });
+    const user = await createUser.execute({ name, email, password });
 
-        // Antes para mostrar o usuário sem a variável password apenas tinhamos colocado "delete user.password" mas com a atualização do TypeScript, as seguintes linhas de código se fazem necessárias.
-        const userWithoutPassword = {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            created_at: user.created_at,
-            updated_at: user.updated_at,
-        };
+    // Antes para mostrar o usuário sem a variável password apenas tinhamos colocado "delete user.password" mas com a atualização do TypeScript, as seguintes linhas de código se fazem necessárias.
+    const userWithoutPassword = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+    };
 
-        return response.json(userWithoutPassword);
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
+    return response.json(userWithoutPassword);
 });
 
 usersRouter.patch(
@@ -38,27 +34,23 @@ usersRouter.patch(
     ensureAuthenticated,
     upload.single('avatar'),
     async (request, response) => {
-        try {
-            const updateUserAvatar = new UpdateUserAvatarService();
+        const updateUserAvatar = new UpdateUserAvatarService();
 
-            const user = await updateUserAvatar.execute({
-                user_id: request.user.id,
-                avatarFileName: request.file.filename,
-            });
+        const user = await updateUserAvatar.execute({
+            user_id: request.user.id,
+            avatarFileName: request.file.filename,
+        });
 
-            const userWithoutPassword = {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                avatar: user.avatar,
-                created_at: user.created_at,
-                updated_at: user.updated_at,
-            };
+        const userWithoutPassword = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+        };
 
-            return response.json(userWithoutPassword);
-        } catch (err) {
-            return response.status(400).json({ error: err.message });
-        }
+        return response.json(userWithoutPassword);
     },
 );
 
