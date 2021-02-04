76ab0471be8762e4101953240f9e51021cf31bdc
ian <iancmilan@unochapeco.edu.br>
2021-02-04 14:03:49 -0300

    página de cadastro 🆕

---
+++ web/src/pages/SignUp/styles.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import signUpBackgroundImg from '../../assets/sign-up-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch; //faz com que os itens que estão dentro desse container ocupem o tamanho total da página
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  place-content: center;
+
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#F4EDE8')};
+      }
+    }
+  }
+
+  // esse > significa que essa estilização é somente para os "a" que estiverem dentro do content, e não mais um nível para dentro, se não fizesse isso iria atrapalhar os "a" de dentro do form
+  > a {
+    color: #f4ede8;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    &:hover {
+      color: ${shade(0.2, '#f4ede8')};
+    }
+
+    svg {
+      margin-right: 16px;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1; //faz com que o background ocupe todo o espaço, menos os 700px do content
+  background: url(${signUpBackgroundImg}) no-repeat center;
+  background-size: cover; //vai garantir que a imagem vai cobrir todo o conteúdo
+`;
