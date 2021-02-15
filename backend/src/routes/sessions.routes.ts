8762f9eeec90d7745b56c25055b34ef0d6bd6d55
ian <iancmilan@unochapeco.edu.br>
2021-02-15 15:04:12 -0300

    ajuste retorno sessions 🔨

--- backend/src/routes/sessions.routes.ts
+++ backend/src/routes/sessions.routes.ts
@@ -15,11 +15,13 @@ sessionsRouter.post('/', async (request, response) => {
     });
 
     const userWithoutPassword = {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        created_at: user.created_at,
-        updated_at: user.updated_at,
+        user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+        },
         token,
     };
 
