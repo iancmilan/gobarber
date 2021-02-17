a57ee0eed4b70755a94689b4a047f0924586b237
ian <iancmilan@unochapeco.edu.br>
2021-02-17 16:57:33 -0300

    animando toasts 📩

--- web/src/components/ToastContainer/Toast/index.tsx
+++ web/src/components/ToastContainer/Toast/index.tsx
@@ -1,10 +1,5 @@
 import React, { useEffect } from 'react';
-import {
-  FiAlertCircle,
-  FiCheckCircle,
-  FiInfo,
-  FiXCircle,
-} from 'react-icons/fi';
+import { FiCheckCircle, FiInfo, FiXCircle } from 'react-icons/fi';
 
 import { ToastMessage, useToast } from '../../../hooks/toast';
 
@@ -12,15 +7,16 @@ import { Container } from './styles';
 
 interface ToastProps {
   message: ToastMessage;
+  style: Record<string, unknown>;
 }
 
 const icons = {
   info: <FiInfo size={24} />,
-  error: <FiAlertCircle size={24} />,
+  error: <FiXCircle size={24} />,
   success: <FiCheckCircle size={24} />,
 };
 
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
   const { removeToast } = useToast();
 
   useEffect(() => {
@@ -41,7 +37,11 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
   }, [removeToast, message.id]);
 
   return (
-    <Container type={message.type} hasDescription={!!message.description}>
+    <Container
+      type={message.type}
+      hasDescription={!!message.description}
+      style={style}
+    >
       {icons[message.type || 'info']}
 
       <div>
